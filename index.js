24baede4b97fa28b506f7fa7481b1682620703c3
machleslie <machariales@gmail.com>
2023-09-26 12:00:50 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching (drivers,driverName){
+    return drivers.filter(function (drivers){
+    return drivers.toLowerCase()=== driverName.toLowerCase();
+    
+});}matchingName
+matchingName
+function fuzzyMatch(drivers,driversFirstTwoLetter){
+    return drivers.filter(function (drivers){
+        return drivers.startsWith(driversFirstTwoLetter);        
+        
+    })};
+
+
+function matchName(drivers,nameProvided){
+    return drivers.filter(function(drivers){
+            return drivers.name === nameProvided;
+        })
+    };
